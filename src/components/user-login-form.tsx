6f29f68d60993479233b1b8147a3112abe9e22d3
Aleksandr Madisson <aleksandr.madisson@dynatrace.com>
2024-07-26 10:32:12 +0300

    auth

---
+++ src/components/user-login-form.tsx
@@ -0,0 +1,99 @@
+'use client';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useSearchParams } from 'next/navigation';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { Button } from '@/components/ui/button';
+import { signIn } from 'next-auth/react';
+
+const formSchema = z.object({
+  email: z.string().email({ message: 'Enter a valid email address' }),
+  password: z.string().min(6, { message: 'Password must be at least 6 characters' })
+});
+
+type UserFormValue = z.infer<typeof formSchema>;
+
+export default function UserLoginForm() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl');
+  const [loading, setLoading] = useState(false);
+  const defaultValues = {
+    email: 'demo@gmail.com',
+    password: ''
+  };
+  const form = useForm<UserFormValue>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  });
+
+  const onSubmit = async (data: UserFormValue) => {
+    console.log(data);
+    signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      callbackUrl: callbackUrl ?? '/dashboard'
+    });
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-2"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button disabled={loading} className="ml-auto w-full" type="submit">
+            Continue With Email
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
